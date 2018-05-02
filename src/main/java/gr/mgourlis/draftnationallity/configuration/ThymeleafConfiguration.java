e927dce26df8c5b295214b0126b3a5b310ef161d
mgourlis <myrgourlis@gmail.com>
2018-05-03 02:39:46 +0300

    2.0.1

--- src/main/java/gr/mgourlis/draftnationallity/configuration/ThymeleafConfiguration.java
+++ src/main/java/gr/mgourlis/draftnationallity/configuration/ThymeleafConfiguration.java
@@ -1,9 +1,18 @@
 package gr.mgourlis.draftnationallity.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+import java.util.Locale;
 
 @Configuration
 public class ThymeleafConfiguration {
@@ -17,6 +26,5 @@ public class ThymeleafConfiguration {
     public SpringSecurityDialect springSecurityDialect() {
         return new SpringSecurityDialect();
     }
-
 
 }
--- src/main/java/gr/mgourlis/draftnationallity/configuration/WebConfiguration.java
+++ src/main/java/gr/mgourlis/draftnationallity/configuration/WebConfiguration.java
@@ -2,15 +2,22 @@ package gr.mgourlis.draftnationallity.configuration;
 
 import gr.mgourlis.draftnationallity.filters.changePasswordFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.List;
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
@@ -37,6 +44,29 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/upload-dir/**").addResourceLocations("classpath:/META-INF/resources/static/upload-dir/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry reg) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("locale");
+        reg.addInterceptor(interceptor);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource msgSrc = new ReloadableResourceBundleMessageSource();
+        msgSrc.setBasename("classpath:i18n/messages");
+        msgSrc.setDefaultEncoding("UTF-8");
+        return msgSrc;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("GR"));
+        resolver.setCookieName("nqI18N_language_cookie");
+        return resolver;
     }
 
 }
